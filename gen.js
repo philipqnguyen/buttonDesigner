13108feac539bf92963513ab9f8d55b2372420ee
Philip Nguyen <supertaru@gmail.com>
2014-08-15 14:02:23 -0700

    Add auto preview

--- gen.js
+++ gen.js
@@ -1,4 +1,4 @@
-
+var buttonText = document.getElementById('button-text');
 
 var style1 = document.getElementById('buttonstyle1');
 var style2 = document.getElementById('buttonstyle2');
@@ -20,7 +20,7 @@ var style_arr = [style1, style2, style3, style4, style5];
 
 var color_arr = [redDesign, orangeDesign, yellowDesign, greenDesign, blueDesign];
 
-
+// Loops through the styles to find the style that was selected
 var style = function () {
   for (i = 0; i < style_arr.length; i++) {
     if (style_arr[i].checked) {
@@ -29,6 +29,7 @@ var style = function () {
   }
 }
 
+// Loops through the colors to find the color that was selected
 var color = function () {
   for (i = 0; i < color_arr.length; i++) {
     if (color_arr[i].checked) {
@@ -37,13 +38,21 @@ var color = function () {
   }
 }
 
-
-
+// Outputs the text and selected color and style into the HTML
 var preview = function() {
-  var buttonText = document.getElementById('button-text').value;
-
   outputVisual.className = color() + " " + style();
-  outputVisual.innerHTML = "<p>" + buttonText + "</p>";
+  outputVisual.innerHTML = "<p>" + buttonText.value + "</p>";
 }
 
-previewButton.addEventListener('click', preview, false);
+// listens for events on text input
+buttonText.addEventListener('keyup', preview, false);
+
+// listens for events on styles
+for (i = 0; i < style_arr.length; i++) {
+  style_arr[i].addEventListener('click', preview, false);
+}
+
+// listens for events on colors.
+for (i = 0; i < color_arr.length; i++) {
+  color_arr[i].addEventListener('click', preview, false);
+}
